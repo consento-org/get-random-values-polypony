8320b7b5a07edf4829a7c7672379cae8f95e4072
Martin Heidegger <martin.heidegger@gmail.com>
2020-05-21 20:21:06 +0900

    doc(types): added support for all types of typed arrays

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 declare module '@consento/sync-randombytes' {
-  export default function <T extends Uint8Array>(input: T): T;
+  export default function <T extends Uint8Array | Uint8ClampedArray | Int8Array | Uint16Array | Int16Array | Uint32Array | Int32Array>(input: T): T;
 }
