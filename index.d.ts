4c53db2690436ca6f89b26d4ef67ee834a7a0757
Martin Heidegger <martin.heidegger@gmail.com>
2020-05-21 20:03:23 +0900

    Removed unnecessary buffer reference.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,3 @@
 declare module '@consento/sync-randombytes' {
-  import { Buffer } from 'buffer'
-
-  export default function <T extends Uint8Array | Buffer>(input: T): T;
+  export default function <T extends Uint8Array>(input: T): T;
 }
