f8965bef4df1e18da2e6cfeb2e6e9d2e55914036
Martin Heidegger <martin.heidegger@gmail.com>
2020-07-02 17:04:20 +0900

    build windows compatibility

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -r dist 2> /dev/null
-mkdir -p dist/test
-cp -r Readme.md LICENSE *.podspec ios android dist
-./babelBuild *.js test/*.js
-
-node -p "pkg = require('./package.json'); delete pkg.private; JSON.stringify(pkg, null, 2)" > dist/package.json
