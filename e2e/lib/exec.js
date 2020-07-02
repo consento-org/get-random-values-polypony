c6e3c3b1f4d617828eff121d016c9eece37cbe61
Martin Heidegger <martin.heidegger@gmail.com>
2020-07-02 15:22:07 +0900

    First attempt: github workflow

---
+++ e2e/lib/exec.js
@@ -0,0 +1,54 @@
+const { spawn } = require('child_process')
+
+const exec = function (command, args, opts) {
+  const spawned = spawn(command, args, {
+    stdio: ['ignore', 'pipe', 'pipe'],
+    ...opts
+  })
+  let buf = ''
+  let trigger
+  const handleData = data => {
+    buf += data.toString()
+    ;trigger && trigger()
+  }
+  spawned.stderr.on('data', handleData)
+  spawned.stdout.on('data', handleData)
+  spawned.on('exit', code => {
+    proc.exit = code
+    ;trigger && trigger()
+  }) 
+
+  const proc = {
+    [Symbol.asyncIterator]: async function * (opts = {}) {
+      while (proc.exit === null || buf.length > 0) {
+        do {
+          const index = buf.indexOf('\n')
+          if (index === -1) {
+            yield buf
+            buf = ''
+          } else {
+            yield buf.substr(0, index)
+            buf = buf.substr(index + 1)
+          }
+        } while (buf.length > 0)
+        await new Promise(resolve => {
+          trigger = resolve
+        })
+      }
+      if (opts.noError !== true && proc.exit !== 0) {
+        throw new Error(`Error: Command ended with exit code: ${proc.exit}`)
+      }
+    },
+    exit: null
+  }
+  return proc
+}
+
+const logExec = async function (command, args, opts) {
+  for await (const line of exec(command, args, opts))
+    console.log(line)
+}
+
+module.exports = {
+  exec, logExec
+}
--- e2e/rntape/index.js
+++ e2e/rntape/index.js
@@ -1,27 +1,3 @@
-/**
- * @format
- */
-// global.Buffer = require('buffer').Buffer
-global.Buffer = class {
-  constructor() {
-    
-  }
-}
-global.Buffer.from = function () {
-  console.log(new Error().stack)
-}
-global.Buffer.allocUnsafe = function () {
-  console.log(new Error().stack)
-}
-/*
-process.browser = true
-process.nextTick = setImmediate
-process.cwd = function () {
-  return '.'
-}
-global.__dirname = '.'
-*/
-
 const { AppRegistry } = require('react-native')
 const App = require('./App')
 const { name: appName } = require('./app.json')
