a141315f76601d68ce319654b0de0bb5f728258d
Martin Heidegger <martin.heidegger@gmail.com>
2020-07-02 22:57:05 +0900

    adding offset support to browser implementation

--- browser.js
+++ browser.js
@@ -1,20 +1,18 @@
 const base = window.crypto || window.msCrypto
 const MAX_SIZE = 65536
 function getRandomValuesBrowser (input) {
-  if (input.byteLength <= MAX_SIZE) {
+  const n = input.byteLength
+  const offset = input.byteOffset
+  const buffer = input.buffer
+  if (n <= MAX_SIZE) {
     base.getRandomValues(
       (input instanceof Float32Array || input instanceof Float64Array || input instanceof DataView || window.msCrypto && input instanceof Uint8ClampedArray)
-      ? new Uint8Array(input.buffer)
+      ? new Uint8Array(buffer, offset, n)
       : base.getRandomValues(input)
     )
   } else {
-    const temp = new Uint8Array(MAX_SIZE)
-    const buffer = input.buffer
-    const inputUint8 = input instanceof Uint8Array ? input : new Uint8Array(buffer)
-    for (let i = 0; i < inputUint8.byteLength; i += MAX_SIZE) {
-      for (let j = 0; j < MAX_SIZE; j++) {
-        inputUint8[i + j] = temp[j]
-      }
+    for (let i = 0; i < n; i += MAX_SIZE) {
+      crypto.getRandomValues(new Uint8Array(buffer, i + offset, Math.min(n - i, MAX_SIZE)))
     }
   }
   return input
