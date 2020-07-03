b0a5c50ddd81e9b473348066ef910a1151c6ff41
Martin Heidegger <martin.heidegger@gmail.com>
2020-07-03 23:16:38 +0900

    Using uniModules for accessing expo data as it will be provided in compiled and test mode.

--- react-native.js
+++ react-native.js
@@ -1,12 +1,8 @@
-const rn = require('react-native')
 const base = window.crypto
 const MAX_SIZE = 65536
-const entropyFromUUID = require('./entropyFromUUID.js')
-const nativeModule = ('GetRandomValuesPolyPony' in rn.NativeModules && rn.NativeModules.GetRandomValuesPolyPony)
-const min = Math.min // babel bug
-
 let impl
 if (base && base.getRandomValues) {
+  const min = Math.min // babel bug
   // Future proofing!
   impl = function getRandomValues (input) {
     if (input.byteLength <= MAX_SIZE) {
@@ -18,25 +14,45 @@ if (base && base.getRandomValues) {
     return input
   }
 } else {
-  console.log({ window.Expo })
-  const seed = entropyFromUUID(
-    ('Expo' in window && window.Expo.Constants.sessionId)
-    || (nativeModule && nativeModule.newUUID())
-  )
-
-  if (seed === null) {
-    throw new Error('Creating Random Seed: No Entropy available to setup random bytes')
+  const rn = require('react-native')
+  const entropyFromUUID = require('./entropyFromUUID.js')
+  const nativeModule = ('GetRandomValuesPolyPony' in rn.NativeModules && rn.NativeModules.GetRandomValuesPolyPony)
+  const uniModules = rn.NativeModules.NativeUnimoduleProxy
+  let seed
+  if (nativeModule) {
+    seed = nativeModule.newUUID()
+  } else {
+    const expoConstants = (uniModules && uniModules.modulesConstants && uniModules.modulesConstants.ExponentConstants)
+    seed = expoConstants && expoConstants.sessionId
+    if (typeof seed !== 'string') {
+      throw new Error('Creating Random Seed: No Entropy available to setup random bytes')
+    }
   }
 
-  impl = require('./createRandomSeed.js')(seed)
+  impl = require('./createRandomSeed.js')(entropyFromUUID(seed))
   
-  if ('Expo' in window) {
-    if ('random' in window.Expo) {
-      window.Expo.random.getRandomBytesAsync(8)
-        .then(
-          bytes => impl.increaseEntropy(bytes),
-          err => console.warn(`[WARNING] Error received when looking for strong entropy, using pretty-strong entropy: ${err}`)
-        )
+  if (!nativeModule) {
+    // Collect more entropy since Expo.sessionId can be read from
+    // other processes quite readily, while nativeModule.newUUID()
+    // is harder to intercept.
+    const uniMethods = uniModules && uniModules.exportedMethods
+    const random = (
+      uniMethods
+      && uniMethods.ExpoRandom
+      && uniMethods.ExpoRandom.find(method => method.name === 'getRandomBase64StringAsync')
+      && function (...args) {
+        return uniModules.callMethod('ExpoRandom', 'getRandomBase64StringAsync', args)
+      }
+    )
+
+    if (random) {
+      // 8 bytes are enough, but increaseEntropy assumes utf-8 encoded entropy, but
+      // random returns base64 encoded entropy, which means we loose some entropy in the conversion.
+      // To counteract ~x4 loss of entropy we collect 32 bit.
+      random(32).then(
+        bytes => impl.increaseEntropy(bytes),
+        err => console.warn(`[WARNING] Error received when looking for strong entropy, using pretty-strong entropy: ${err}`)
+      )
     } else {
       console.warn(`[WARNING] No means to retreive very strong entropy, using pretty-strong entropy.`)
     }
