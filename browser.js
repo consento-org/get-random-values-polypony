19fae85db72f7b723b952b8ebe52fb7d605a4cf1
Martin Heidegger <martin.heidegger@gmail.com>
2020-07-04 03:14:02 +0900

    Adding support for BigUint64 and BigUint64 support to browser and cleanly implement browser with less complexity

--- browser.js
+++ browser.js
@@ -1,33 +1,63 @@
-const base = window.crypto || window.msCrypto
 const MAX_SIZE = 65536
+let base
+let getRandomValues
+let polyfill
+if (window.msCrypto || !!window.StyleMedia) { // IE or Metro
+  base = window.msCrypto || window.crypto
+  const randomValues = base.getRandomValues
+  getRandomValues = function (input) {
+    if (input instanceof Uint8ClampedArray) {
+      randomValues.call(base, new Uint8Array(input.buffer, input.byteOffset, input.byteLength))
+    } else {
+      randomValues.call(base, input)
+    }
+    return input
+  }
+  polyfill = function () {
+    if (!window.crypto) {
+      window.crypto = {}
+    }
+    if (window.crypto.getRandomValues !== getRandomValues) {
+      window.crypto._getRandomValues = window.crypto.getRandomValues
+      window.crypto.getRandomValues = getRandomValues
+    }
+  }
+} else {
+  base = window.crypto
+  polyfill = function () {}
+  getRandomValues = base.getRandomValues
+}
 function getRandomValuesBrowser (input) {
   const n = input.byteLength
   const offset = input.byteOffset
   const buffer = input.buffer
   if (n <= MAX_SIZE) {
-    base.getRandomValues(
-      (input instanceof Float32Array || input instanceof Float64Array || input instanceof DataView || ((window.msCrypto || !!window.StyleMedia) && input instanceof Uint8ClampedArray))
-        ? new Uint8Array(buffer, offset, n)
-        : base.getRandomValues(input)
+    getRandomValues.call(
+      base,
+      (
+        input instanceof Float32Array ||
+        input instanceof Float64Array ||
+        input instanceof DataView ||
+        (
+          window.BigInt64Array &&
+          (
+            input instanceof window.BigInt64Array ||
+            input instanceof window.BigUint64Array
+          )
+        )
+      )
+      ? new Uint8Array(buffer, offset, n)
+      : input
     )
   } else {
     for (let i = 0; i < n; i += MAX_SIZE) {
-      base.crypto.getRandomValues(new Uint8Array(buffer, i + offset, Math.min(n - i, MAX_SIZE)))
+      getRandomValues.call(base, new Uint8Array(buffer, i + offset, Math.min(n - i, MAX_SIZE)))
     }
   }
+
   return input
 }
 getRandomValuesBrowser.name = 'getRandomValuesBrowser' // IE 11
-getRandomValuesBrowser.polyfill = function () {
-  if (window.crypto === undefined) {
-    // Internet explorer 11 should get the getRandomValues method
-    // as well, if polyfill is required.
-    window.crypto = {
-      getRandomValues: function (input) {
-        return base.getRandomValues(input)
-      }
-    }
-  }
-}
+getRandomValuesBrowser.polyfill = polyfill
 
 module.exports = getRandomValuesBrowser
--- browserLimitations.js
+++ browserLimitations.js
@@ -1,18 +1,26 @@
+const BigInt64Array = global.BigInt64Array || class {}
+const BigUint64Array = global.BigUint64Array || class {}
+
 module.exports = function (getRandomValues) {
   const name = getRandomValues.name + 'Limited'
   const impl = function (input) {
-    let wrongType
-    if (input instanceof Float32Array) {
-      wrongType = 'Float32'
-    }
-    if (input instanceof Float64Array) {
-      wrongType = 'Float64'
-    }
-    if (input instanceof DataView) {
-      wrongType = 'DataView'
-    }
-    if (wrongType !== undefined) {
-      throw new Error('Failed to execute \'getRandomValues\' on \'Crypto\': The provided ArrayBufferView is of type \'' + wrongType + '\', which is not an integer array type.')
+    // Quick checking for most common case
+    if (!(input instanceof Uint8Array)) {
+      let wrongType
+      if (input instanceof Float32Array) {
+        wrongType = 'Float32'
+      } else if (input instanceof Float64Array) {
+        wrongType = 'Float64'
+      } else if (input instanceof DataView) {
+        wrongType = 'DataView'
+      } else if (input instanceof BigInt64Array) {
+        wrongType = 'BigInt64'
+      } else if (input instanceof BigUint64Array) {
+        wrongType = 'BigUint64'
+      }
+      if (wrongType !== undefined) {
+        throw new Error('Failed to execute \'getRandomValues\' on \'Crypto\': The provided ArrayBufferView is of type \'' + wrongType + '\', which is not an integer array type.')
+      }
     }
     if ('byteLength' in input && input.byteLength > 65536) {
       throw new Error('Failed to execute \'getRandomValues\' on \'Crypto\': The ArrayBufferView\'s byte length (' + input.byteLength + ') exceeds the number of bytes of entropy available via this API (65536).')
